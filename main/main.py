5d074eebc4218010bdee89d2accb5fddfccbef40
Diego Barletta <diegohbarletta@gmail.com>
2026-07-01 18:55:22 -0300

    commit de proyecto

---
+++ main/main.py
@@ -0,0 +1,181 @@
+# Este programa permite:
+#   -agregar productos
+#   -ver productos
+
+import sqlite3
+import os
+
+# ---------------------
+# Conexion y creacion de tabla
+# ---------------------
+def conectar_db():
+    """
+    
+    """
+    conexion = sqlite3.connect("inventario.db")
+    cursor = conexion.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS productos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        categoria TEXT NOT NULL,
+        precio REAL NOT NULL
+        )
+    """)
+    conexion.commit()
+    return conexion
+
+# ---------------------
+# OPERACIONES CRUD 
+# ---------------------
+def agregar_producto(conexion):
+    """Pide datos al usuario y los guarda en la base de datos."""
+    print("\n--- Nuevo producto")
+    while True:
+        nombre = input(" Nombre: ").strip()
+        if nombre:
+            break
+        print("❌ El nombre no puede estar vacio")
+    while True:
+            categoria = input(" categoria: ").strip()
+            if categoria:
+                break
+            print("❌ La categoria no puede estar vacio")
+    while True:
+        try:
+            precio = float(input(" Precio: $").replace(",","."))
+            if precio > 0:
+                break
+            print(" ❌ El precio debe ser mayor a 0 ")
+        except ValueError:
+            print(" ❌ Ingresa un numero valido (ej: 1200.40)")
+    cursor = conexion.cursor()
+    try:
+        cursor.execute("INSERT INTO productos (nombre,categoria,precio) VALUES (?,?,?) ",(nombre,categoria,precio))
+        conexion.commit()
+        print(f" ✅ Producto '{nombre}' agregado correctamente.")
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print(f"\n ❌ Error al guardar: {e}")
+
+def ver_productos(conexion):
+    """Muestra todos os productos en formato tabla."""
+    cursor = conexion.cursor()
+    cursor.execute("SELECT id,nombre,categoria,precio FROM productos ORDER BY id")
+    filas = cursor.fetchall()
+
+    if not filas:
+        print("\n no hay productos cargados.")
+        return
+
+    print("\n" + "-" * 60)
+    print(f"  {'ID':<5} {'NOMBRE':<20} {'CATEGORÍA':<15} {'PRECIO':>15}")
+    print("-" * 60)
+    for id_, nombre, categoria, precio in filas:
+        print(f"  {id_:<5} {nombre:<20} {categoria:<15} ${precio:>14,.2f}")
+    print("-" * 60)
+    print(f"  Total de productos: {len(filas)}")
+    
+def buscar_productos(conexion):
+    """ Buscar productos por su nombre"""
+    termino = input("\nIngresa el nombre a buscar: ").strip()
+    if not termino:
+        print(" ❌ El termino de busqueda no puede estar vacio")
+        return
+
+    cursor = conexion.cursor()
+    cursor.execute("SELECT id,nombre,categoria,precio FROM productos WHERE nombre LIKE ? ORDER BY id", (f"%{termino}%",) )
+    filas = cursor.fetchall()
+
+    if not filas:
+        print(f" No se encontraron los productos con este {termino}")
+        return
+
+    print(f"\n  Se encontraron {len(filas)} resultado(s):")
+    for id_, nombre, categoria, precio in filas:
+        print(f"    ID: {id_} | {nombre} | {categoria} | ${precio:,.2f}")
+    
+def eliminar_productos(conexion):
+    """Elimina productos por su id"""
+    ver_productos(conexion)
+
+    try:
+        id_eliminar = int(input("\n Ingresa el ID del producto a eliminar: ").strip()) # 1
+    except ValueError:
+        print(" ❌ el id debe ser un numero entero")
+        return
+
+    cursor = conexion.cursor()
+    cursor.execute("SELECT nombre FROM productos WHERE id = ? " , (id_eliminar,)) # id=1 nombre="coca-cola" cat="ada" precio=1231 -> ["1","coca-cola","categora",1231432]
+    fila = cursor.fetchone()
+
+    if not fila:
+        print(f" ❌ No existe ningun producto con el id {id_eliminar}")
+        return
+
+    nombre = fila[0]
+    print(f"\n Producto encontrado: {nombre}")
+    respuesta = input(f" ¿ Confirmas que queres eliminar '{nombre}' (s/n): ").strip()
+    if respuesta != 's':
+        print("Operacion cancelada.")
+        return
+
+    try:
+        cursor.execute("DELETE FROM productos WHERE id = ?",(id_eliminar,))
+        conexion.commit()
+        print(f"✅ producto '{nombre}' eliminado correctamente.")
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print(f"\n Error al eliminar: {e}")
+
+# ---------------------
+# MENU PRINCIPAL
+# ---------------------
+
+def limpiar_pantalla():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def pausar():
+    input("\n Presioná Enter para continuar...")
+
+def main():
+    conexion = conectar_db()
+
+    while True:
+        limpiar_pantalla()
+        print("\n" + "=" * 50)
+        print("     SISTEMA DE GESTIÓN DE PRODUCTOS")
+        print("=" * 50)
+        print("  1. Agregar producto")
+        print("  2. Ver todos los productos")
+        print("  3. Buscar producto por nombre")
+        print("  4. Eliminar producto")
+        print("  5. Salir")
+        print("-" * 50)
+
+        opcion = input("Selecciona una opcion (1-5): ").strip()
+
+        if opcion == "1":
+            agregar_producto(conexion)
+            pausar()
+        elif opcion == "2":
+            ver_productos(conexion)
+            pausar()
+        elif opcion == "3":
+            buscar_productos(conexion)
+            pausar()
+        elif opcion == "4":
+            eliminar_productos(conexion)
+            pausar()
+        elif opcion == "5":
+            print("\n Hasta luego")
+            break
+        else:
+            print(" Opcion invalida. Igresa un numero del 1 al 5")
+            pausar()
+
+
+    conexion.close()
+
+if __name__ == "__main__":
+    main()
